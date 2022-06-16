e26ce32ea1723c3b47d6b0e99f7bf71eea2d00e5
k3shav17 <keshavrao0489@gmail.com>
2022-06-16 16:05:58 +0530

    post order method added in controller, no implementation yet

--- src/main/java/com/commerce/controller/OrderController.java
+++ src/main/java/com/commerce/controller/OrderController.java
@@ -1,9 +1,15 @@
 package com.commerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.commerce.model.Order;
 import com.commerce.repository.OrderRepository;
 
 @RestController
@@ -12,4 +18,10 @@ public class OrderController {
 
 	@Autowired
 	OrderRepository orderRepository;
+
+	@PostMapping("/buy")
+	private ResponseEntity<Object> createOrder(@RequestBody Order order) {
+		return new ResponseEntity<>("Product ordered successfully ", new HttpHeaders(), HttpStatus.OK);
+	}
+
 }
